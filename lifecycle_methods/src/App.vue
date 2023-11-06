25b1f24e9416d797115b6cbaf515d829105e86a0
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-06 17:03:03 +0545

    lifecycle_methods

---
+++ lifecycle_methods/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import lifecycle from './components/lifecycle.vue';
+</script>
+
+<template>
+  <lifecycle/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ lifecycle_methods/src/components/lifecycle.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>beforeCreate and created</h1>
+    <h2>beforeMount and mounted</h2>
+    <h1 ref="counter"> {{ count }}</h1>
+    <button @click="count=count+1">Update Count</button>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+        count:0
+    }
+  },
+   
+        beforeCreate(){
+            alert("before create")
+        },
+
+        created(){
+            alert("created")
+        },
+
+        beforeMount(){
+            console.log("beforeMount",this.$el)
+        },
+        mounted(){
+            console.log("  mounted",this.$el)
+        },
+
+        beforeUpdate(){
+            console.log("beforeUpdate",this.$refs['counter'].textContent)
+        },
+
+        updated(){
+            console.log("  updated",this.$refs['counter'].textContent)
+        }
+   
+}
+</script>
